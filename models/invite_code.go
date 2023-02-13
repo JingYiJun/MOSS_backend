955be852d1b0361a3bb2f883a506372d62dc4dc4
Ke Chen <2697947313@qq.com>
2023-02-13 18:25:19 +0800

    fix: invite code primary key

--- models/invite_code.go
+++ models/invite_code.go
@@ -1,7 +1,7 @@
 package models
 
 type InviteCode struct {
-	ID          string `gorm:"primaryKey"`
+	ID          int    `gorm:"primaryKey"`
 	Code        string `gorm:"unique,size:32"`
 	IsSend      bool
 	IsActivated bool
