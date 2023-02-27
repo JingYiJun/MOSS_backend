aa4abd1bb79733cf6bc90f6c0b9fa2f005817f5f
Ke Chen <2697947313@qq.com>
2023-02-27 16:16:09 +0800

    fix: html format

--- apis/chat/image_test.go
+++ apis/chat/image_test.go
@@ -10,6 +10,7 @@ func TestGenerateImage(t *testing.T) {
 	buf, err := GenerateImage([]models.RecordModel{
 		{"111", "```func\n    func\n    func\n```"},
 		{"111", "```func\nfunc\nfunc\n```"},
+		{"hi", "My name is MOSS, created by the FudanNLP Lab in the School of Computer Science at Fudan University."},
 	})
 	if err != nil {
 		t.Fatal(err)
