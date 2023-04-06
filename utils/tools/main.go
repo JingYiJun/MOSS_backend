def63aa60580e2032f4d76773c0f8bdb879708af
cgruixiao <cgruixiao@outlook.com>
2023-04-06 11:46:28 +0800

    feat: order: search, calculate, solve, draw; maxCommandNumber set up to 4 as we have 4 kinds of tools

--- utils/tools/main.go
+++ utils/tools/main.go
@@ -4,37 +4,44 @@ import (
 	"MOSS_backend/config"
 	"fmt"
 	"regexp"
+	"sort"
 	"strings"
 )
 
 type Map = map[string]any
 
-var commandSplitRegexp = regexp.MustCompile(`\w+\("[\s\S][\s\S]*?"\)`)
+const maxCommandNumber = 4
+
+var commandSplitRegexp = regexp.MustCompile(`(Search|Solve|Calculate|Draw)\("([\s\S]+?)"\)`)
+var commandOrder = map[string]int{"Search": 1, "Calculate": 2, "Solve": 3, "Draw": 4}
 
 func Execute(rawCommand string) (string, any) {
 	if rawCommand == "None" || rawCommand == "none" {
 		return "None", nil
 	}
-
-	commands := commandSplitRegexp.FindAllString(rawCommand, -1)
+	// commands is like: [[Search("A"), Search, A,] [Solve("B"), Solve, B] [Search("C"), Search, C]]
+	commands := commandSplitRegexp.FindAllStringSubmatch(rawCommand, -1)
 
 	var extraDataSlice = make([]map[string]any, 0)
 	if len(commands) == 0 {
 		return "None", extraDataSlice
 	}
-	for i := range commands {
-		commands[i] = strings.Trim(commands[i], " ")
-	}
+	// sort, search should be at first
+	sort.Slice(commands, func(i, j int) bool {
+		return commandOrder[commands[i][1]] < commandOrder[commands[j][1]]
+	})
+	// commands now like: [[Search("A"), Search, A,] [Search("C"), Search, C] [Solve("B"), Solve, B]]
 	var resultsBuilder strings.Builder
+	searchResultsIndex := 0
 	for i := range commands {
-		if i > 1 {
+		if i >= maxCommandNumber {
 			break
 		}
 		if i > 0 { // separator is '\n'
 			resultsBuilder.WriteString("\n")
 		}
-		results, extraData := executeOnce(commands[i])
-		resultsBuilder.WriteString(commands[i])
+		results, extraData := executeOnce(commands[i][1], commands[i][2], &searchResultsIndex)
+		resultsBuilder.WriteString(commands[i][0])
 		resultsBuilder.WriteString(" =>\n")
 		resultsBuilder.WriteString(results)
 		if extraData != nil {
@@ -47,14 +54,15 @@ func Execute(rawCommand string) (string, any) {
 	return resultsBuilder.String(), extraDataSlice
 }
 
-func executeOnce(command string) (result string, extraData map[string]any) {
+func executeOnce(action string, args string, searchResultIndex *int) (string, map[string]any) {
 	if config.Config.Debug {
-		fmt.Println(command)
+		fmt.Println(action + args)
 	}
-	action, args := cutCommand(command)
 	switch action {
 	case "Search":
-		return search(args)
+		results, extraData := search(args)
+		searchResult := searchResultsFormatter(results, searchResultIndex)
+		return searchResult, extraData
 	case "Calculate":
 		return calculate(args)
 	case "Solve":
@@ -66,11 +74,11 @@ func executeOnce(command string) (result string, extraData map[string]any) {
 	}
 }
 
-func cutCommand(command string) (string, string) {
-	before, after, found := strings.Cut(command, "(")
-	if found {
-		return before, strings.Trim(after, "\")")
-	} else {
-		return command, ""
-	}
-}
+//func cutCommand(command string) (string, string) {
+//	before, after, found := strings.Cut(command, "(")
+//	if found {
+//		return before, strings.Trim(after, "\")")
+//	} else {
+//		return command, ""
+//	}
+//}
