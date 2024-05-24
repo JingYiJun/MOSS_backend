8f61c4ab764326468d90565b25bb5559bb4f295b
Ke Chen <JingYiJun3104@outlook.com>
2024-05-24 15:29:27 +0800

    feat: change routes

--- apis/record/routes.go
+++ apis/record/routes.go
@@ -26,5 +26,5 @@ func RegisterRoutes(routes fiber.Router) {
 	routes.Post("/v1/chat/completions", OpenAICreateChatCompletion)
 
 	// yocsef API
-	routes.Get("/yocsef/inference", websocket.New(InferYocsefAsyncAPI))
+	routes.Get("/ws/yocsef/inference", websocket.New(InferYocsefAsyncAPI))
 }
